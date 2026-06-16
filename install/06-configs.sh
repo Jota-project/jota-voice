925a2abac0dbd0473af45704b86f6f30142155f1
Sito <alfonsogarrelopez@gmail.com>
2026-06-16 20:57:28 +0200

    feat(install): 06-configs.sh crea ~/.jota-display-url

--- install/06-configs.sh
+++ install/06-configs.sh
@@ -1,14 +1,20 @@
 #!/bin/sh
 set -e
 source ../lib/output.sh
+source ../lib/yaml.sh
 
 _check() {
-    [ -f "$HOME/supervisord.conf" ]
+    [ -f "$HOME/supervisord.conf" ] && [ -f "$HOME/.jota-display-url" ]
 }
 
 _apply() {
     cp "$REPO_DIR/boot/supervisord.conf.tpl" "$HOME/supervisord.conf"
     _ok "supervisord.conf generado"
+
+    local display_url
+    display_url=$(yaml_get display.url || echo 'http://127.0.0.1:8766')
+    echo "$display_url" > "$HOME/.jota-display-url"
+    _ok "display URL guardada: $display_url"
 }
 
 if [ -n "$1" ] && [ "$1" = "--check" ]; then
